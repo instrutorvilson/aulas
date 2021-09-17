1fa56259b3a8522b4f015493891cafb6ef12c9b5
vilsonmoro <vilsonmoro@gmail.com>
2021-09-17 18:19:15 -0300

    Add ProductController with method for search by Id

---
+++ CadastroProduto/src/main/java/com/aulas/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.aulas.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aulas.entites.Product;
+import com.aulas.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+	
+	@Autowired
+	ProductService service;
+	
+	@GetMapping( value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id){
+		Product product = service.findById(id);
+		return ResponseEntity.ok(product);
+	}
+
+}
--- CadastroProduto/src/main/java/com/aulas/services/ProductService.java
+++ CadastroProduto/src/main/java/com/aulas/services/ProductService.java
@@ -20,5 +20,4 @@ public class ProductService {
 		Product product = obj.orElseThrow(null);
 		return product;		
 	}
-
 }
