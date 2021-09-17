f0d8d2d7a4c5cfbaa068c4f0cd1869e28bb108d1
vilsonmoro <vilsonmoro@gmail.com>
2021-09-17 16:57:22 -0300

    Create project cadastro produtos

---
+++ CadastroProduto/src/main/java/com/aulas/AulasApplication.java
@@ -0,0 +1,13 @@
+package com.aulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasApplication.class, args);
+	}
+
+}
---
+++ CadastroProduto/src/main/java/com/aulas/entites/Produto.java
@@ -0,0 +1,16 @@
+package com.aulas.entites;
+
+public class Produto {
+    Long id;
+    String descricao;
+    float preco;
+    float estoque;
+    
+	public Produto() {
+		
+	}
+	
+	
+    
+    
+}
