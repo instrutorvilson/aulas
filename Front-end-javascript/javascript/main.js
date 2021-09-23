f7d99d340b1f56fa858ad29c30b42e4be3937174
vilsonmoro <vilsonmoro@gmail.com>
2021-09-23 10:57:22 -0300

    Build function insert product with fetch api

--- Front-end-javascript/javascript/main.js
+++ Front-end-javascript/javascript/main.js
@@ -1,7 +1,35 @@
+//const baseUrl = "https://management-inventory.herokuapp.com/products";
+//const baseUrl = "http://localhost:8080/products";
+const baseUrl = "http://localhost:3000/products"
 
+function inserir() {
+    var description = document.getElementsByName("description")[0].value;
+    var price = document.getElementsByName("price")[0].value;
+    var inventory = document.getElementsByName("inventory")[0].value;
+
+    var product = {
+        description: description,
+        price: price,
+        inventory: inventory
+    }
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(product),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    fetch(baseUrl, options)
+        .then(response => response.text())
+        .catch((error) =>
+            console.log("Não foi possivel consultar. Erro: " + error.message)
+        )
+}
 
 function getAllLocal() {
-    fetch("http://localhost:3000/products")
+    fetch(baseUrl)
         .then(response => response.json())
         .then(dados => showInTable(dados))
         .catch((error) =>
@@ -17,6 +45,6 @@ function showInTable(content) {
         linha.appendChild(document.createElement("td")).innerHTML = product.description;
         linha.appendChild(document.createElement("td")).innerHTML = product.price;
         linha.appendChild(document.createElement("td")).innerHTML = product.inventory;
-        root.appendChild(linha);        
+        root.appendChild(linha);
     }
 }
