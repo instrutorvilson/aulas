0609cdf669123c74e00855ccd1d14c9b239e00f6
vilsonmoro <vilsonmoro@gmail.com>
2021-09-24 15:15:05 -0300

    Fixing insert and update

--- Front-end-javascript/javascript/main.js
+++ Front-end-javascript/javascript/main.js
@@ -1,8 +1,7 @@
-//const baseUrl = "https://management-inventory.herokuapp.com/products";
+const baseUrl = "http://management-inventory.herokuapp.com/products";
 //const baseUrl = "http://localhost:8080/products";
-const baseUrl = "http://localhost:3000/products"
-
-function inserir() {
+//const baseUrl = "http://localhost:3000/products"
+function inserir() {    
     var description = document.getElementsByName("description")[0].value;
     var price = document.getElementsByName("price")[0].value;
     var inventory = document.getElementsByName("inventory")[0].value;
@@ -15,25 +14,28 @@ function inserir() {
 
     const options = {
         method: 'POST',
+        mode: 'cors',
         body: JSON.stringify(product),
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
         }
     }
 
     fetch(baseUrl, options)
         .then(response => response.text())
+        .then(data => window.location.href("consulta.html"))
         .catch((error) => {
             console.log("Não foi possivel consultar. Erro: " + error.message);
-            return false;
-        }
+         }
 
         )
-    return true;
+    
 }
 
 function alterar() {
-    var idproduct = document.getElementsByName("idproduct")[0].value; 
+    debugger;
+    var idproduct = document.getElementsByName("idproduct")[0].value;
     var description = document.getElementsByName("description")[0].value;
     var price = document.getElementsByName("price")[0].value;
     var inventory = document.getElementsByName("inventory")[0].value;
@@ -47,25 +49,25 @@ function alterar() {
 
     const options = {
         method: 'PUT',
+        mode: 'cors',
         body: JSON.stringify(product),
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
         }
     }
 
-    fetch(baseUrl+"/"+idproduct, options)
+    fetch(baseUrl, options)
         .then(response => response.text())
-        .then( data => window.location.href = "consulta.html")
+        .then(data => window.location.href = "consulta.html")
         .catch((error) => {
-            console.log("Não foi possivel consultar. Erro: " + error.message);
-            return false;
+            console.log("Não foi possivel consultar. Erro: " + error.message);           
         }
 
-        )
-    return true;
+    )
 }
 
-function getAllLocal() {
+function getAll() {
     fetch(baseUrl)
         .then(response => response.json())
         .then(dados => showInTable(dados))
@@ -83,29 +85,29 @@ function showInTable(content) {
         linha.appendChild(document.createElement("td")).innerHTML = product.description;
         linha.appendChild(document.createElement("td")).innerHTML = product.price;
         linha.appendChild(document.createElement("td")).innerHTML = product.inventory;
-        linha.appendChild(document.createElement("td")).innerHTML = "<a href='edita.html?id="+product.id+"'>Editar</a>";
-       
+        linha.appendChild(document.createElement("td")).innerHTML = "<a href='edita.html?id=" + product.id + "'>Editar</a>";
+
         root.appendChild(linha);
     }
 }
 
-function getProductById(id){
-    fetch(baseUrl+"/"+id)
-    .then(response => response.json())
-    .then(dados => showInForm(dados))
-    .catch((error) =>
-        console.log("Não foi possivel consultar. Erro: " + error.message)
-    ) 
+function getProductById(id) {
+    fetch(baseUrl + "/" + id)
+        .then(response => response.json())
+        .then(dados => showInForm(dados))
+        .catch((error) =>
+            console.log("Não foi possivel consultar. Erro: " + error.message)
+        )
 }
 
-function showInForm(content){
+function showInForm(content) {
     document.getElementsByName("idproduct")[0].value = content.id;
     document.getElementsByName("description")[0].value = content.description;
     document.getElementsByName("price")[0].value = content.price;
     document.getElementsByName("inventory")[0].value = content.inventory;
-} 
+}
 
-function limparCampos(){
+function limparCampos() {
     document.getElementsByName("description")[0].value = "";
     document.getElementsByName("price")[0].value = "";
     document.getElementsByName("inventory")[0].value = "";
