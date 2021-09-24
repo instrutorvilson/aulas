8a73586e36493411111922b9fcd814787349e3e3
vilsonmoro <vilsonmoro@gmail.com>
2021-09-24 07:46:39 -0300

    Create project Agenda

---
+++ Agenda/src/main/java/com/agenda/entities/Contato.java
@@ -0,0 +1,56 @@
+package com.agenda.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Contato implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	private String nome;
+    private String fone;
+    private String email;
+    
+    
+	public Contato() {
+		
+	}
+	
+	public Contato(Long id, String nome, String fone, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.fone = fone;
+		this.email = email;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getFone() {
+		return fone;
+	}
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+     
+}
