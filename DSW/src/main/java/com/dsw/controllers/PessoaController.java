717fec09a36c39845a07a2fb7ebe703121de53dc
vilsonmoro <vilsonmoro@gmail.com>
2021-09-28 11:11:48 -0300

    Add request with pathvariable

--- DSW/src/main/java/com/dsw/controllers/PessoaController.java
+++ DSW/src/main/java/com/dsw/controllers/PessoaController.java
@@ -1,6 +1,7 @@
 package com.dsw.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/persons")
 public class PessoaController {
    
+	/*chamada no browser: http://localhost:8080/persons?name=Vilson&idade=45*/
 	@GetMapping
 	public String ola(@RequestParam String name, @RequestParam int idade ) {
-		
-		
 		return "Olá " + name + " você tem " + idade + " anos.";
 	}
+	
+	/*chamada no browser: http://localhost:8080/persons/Vilson*/
+	@GetMapping("/{name}")
+	public String ola(@PathVariable String name) {
+		return "Olá " + name ;
+	}
+	
 	
 }
