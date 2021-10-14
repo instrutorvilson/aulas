c602ff05c733a3b9824e9236a2768f83892f3f16
vilsonmoro <vilsonmoro@gmail.com>
2021-10-14 14:30:55 -0300

    add component sidenav

--- Front-end-agenda/app-agenda/src/app/app.module.ts
+++ Front-end-agenda/app-agenda/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './src/components/header/header/header.component';
+import { SideBarComponent } from './src/components/side-bar/side-bar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
